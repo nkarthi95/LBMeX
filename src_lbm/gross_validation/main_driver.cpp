8f33ed36bb8a2cb29b6cb6d6c12c39ac997f4356
Nikhil Karthikeyan <nikhil@gaira.dhcp.lbl.gov>
2024-07-23 11:36:05 -0700

    fixed homoegenous case. Moving to interface case

--- src_lbm/gross_validation/main_driver.cpp
+++ src_lbm/gross_validation/main_driver.cpp
@@ -34,6 +34,7 @@ inline Vector<std::string> VariableNames(const int numVars) {
   var_names[cnt++] = "rho";
   // Print() << "Density specified\n";
   // velx, vely, velz
+  if(cnt >= numVars){return var_names;}
   for (int d=0; d<AMREX_SPACEDIM; d++) {
     name = "u";
     name += (120+d);
@@ -134,39 +135,40 @@ void main_driver(const char* argv) {
   MultiFab fnew(ba, dm, nvel, nghost);
   MultiFab hydrovs(ba, dm, nvel, nghost);
   MultiFab noise(ba, dm, nvel, nghost);
+  MultiFab rho0(ba, dm, 1, nghost);
   // MultiFab ic_setup(ba, dm, nvel, nghost);
   // MultiFab test_noise(ba, dm, 2*nvel, nghost);
-  Real rho0;
+  // Real rho0;
   if (init_cond == 0) {
-    LBM_init_liquid(fold, hydrovs);
-    rho0 = rhol;
+    LBM_init_liquid(fold, hydrovs, rho0);
+    // rho0 = rhol;
     }
   else if (init_cond == 1){
-    LBM_init_mixture(fold, hydrovs);
+    LBM_init_mixture(fold, hydrovs, rho0);
     rho0 = 0.5*rhol + 0.5*rhov;
     }
   else if (init_cond == 2){
-    LBM_init_flat_interface(geom, fold, hydrovs);
-    rho0 = 0.5*rhol + 0.5*rhov;
+    LBM_init_flat_interface(geom, fold, hydrovs, rho0);
+    // rho0 = 0.5*rhol + 0.5*rhov;
     }
   else if (init_cond == 3){
-    LBM_init_droplet(radius, geom, fold, hydrovs);
-    rho0 = 0.5*rhol + 0.5*rhov;
+    LBM_init_droplet(radius, geom, fold, hydrovs, rho0);
     }
-  // Print() << "Data structures generated\n";
+  Print() << "Data structures generated\n";
+  const Vector<std::string> rho0_varname = VariableNames(1);
+  WriteSingleLevelPlotfile("reference_densities", rho0, rho0_varname, geom, Real(0), 0);
 
   int nStructVars = 10;
   const Vector<std::string> var_names = VariableNames(nStructVars);
-  // Print() << "SF names specified\n";
+  Print() << "SF names specified\n";
   Vector<Real> var_scaling(nStructVars*(nStructVars+1)/2);
   for (int i=0; i<var_scaling.size(); ++i) {
-    if (temperature>0) var_scaling[i] = rho0*temperature; else var_scaling[i] = 1.;
+    if (temperature>0) var_scaling[i] = temperature; else var_scaling[i] = 1.;
   }
   StructFact structFact(ba, dm, var_names, var_scaling);
   // Print() << "StructFact object generated\n";
   // INITIALIZE
   // Print() << "Initial condition created\n";
-  // Print() << "thermodynamic cs2: " << system_cs2 << "\n";
 
   // Write a plotfile of the initial data if plot_int > 0
   plt_name = "hydro_plt";
@@ -175,7 +177,7 @@ void main_driver(const char* argv) {
 
   // TIMESTEP
   for (int step=1; step <= nsteps; ++step) {
-    LBM_timestep(geom, fold, fnew, hydrovs, noise);
+    LBM_timestep(geom, fold, fnew, hydrovs, noise, rho0);
     if (plot_int > 0 && step%plot_int ==0) {
       plt_name = "hydro_plt";
       WriteOutput(step, hydrovs, geom, plt_name);
