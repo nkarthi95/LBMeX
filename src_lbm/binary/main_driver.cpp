c20d7b87b2d84c2835e975a68c230926c08325a5
Ulf Schiller <uschill@udel.edu>
2025-01-01 02:44:49 -0500

    Reinstate reading of parameters from input file

--- src_lbm/binary/main_driver.cpp
+++ src_lbm/binary/main_driver.cpp
@@ -28,7 +28,7 @@ inline void ReadInput() {
   pp.query("plot_int", plot_int);
 
   /* binary fluid parameters */
-  pp.query("chi", chi);
+  pp.query("lambda", chi);
   pp.query("T", T);
   pp.query("kappa", kappa);
   pp.query("gamma", Gamma);
