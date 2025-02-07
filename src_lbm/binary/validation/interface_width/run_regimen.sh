453cc63e528f7c1f7f542c5cacc74594f5668c0c
Nikhil Karthikeyan <nkarthi@uschill-lab1.cis.udel.edu>
2025-02-06 22:37:26 -0500

    Changes 1. Verified theoretical expressions for coexistence densities, surface tension and interface widths 2. Modified LBM_binary.H with new expressions for coexistence densities and interface width in droplet and interface IC's 3. Verified models conforms to expected theoretical values using validation/young_laplace and validation/interface_width

---
+++ src_lbm/binary/validation/interface_width/run_regimen.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#setup from the perspective of the folder it is run in
+amrex_exec="../../../../spatially_independent/main3d.gnu.MPI.ex"
+
+# Defines the radius of the droplet as a proportion of system size. 0.5 is maximum
+chi_s=("2.25" "2.5" "2.75" "3" "3.25") #"3.5" "3.75" "4" "4.25" "4.5" "4.75" "5")
+T="0.2"
+kappa="0.03"
+
+# Define property to be looped over and edited in input file
+search_pattern2="chi = 0.45"
+search_pattern3="kappa = 0.03"
+
+for chi in "${chi_s[@]}"; do
+
+base_folder="chi_${chi}/kappa_${kappa}"
+
+if [ ! -d "$base_folder" ]; then
+  echo "$base_folder does not exist. Creating $base_folder"
+  mkdir -p "$base_folder"
+fi
+
+cd $base_folder
+echo ${PWD}
+
+cp ../../inputs_interface_width .
+
+if [ -e "checkpoint_000020000" ]; then
+  echo "Run complete"
+  cd ..
+  continue
+else
+  echo "Executing commands in $base_folder"
+  # EDIT COMMANDS HERE TO MAKE MODIFICATIONS TO RUNS #
+  lambda=$(echo "$chi * $T" | bc -l)
+  replacement_text="chi = ${lambda}"
+  sed -i "s/$search_pattern2/$replacement_text/g" inputs_interface_width # edits input file with appropriate setting
+  replacement_text="kappa = ${kappa}"
+  sed -i "s/$search_pattern2/$replacement_text/g" inputs_interface_width # edits input file with appropriate setting
+
+  mpirun -n 4 $amrex_exec inputs_interface_width > output.txt
+  # EDIT COMMANDS HERE TO MAKE MODIFICATIONS TO RUNS #
+fi
+cd ../..
+done
--- src_lbm/binary/validation/young_laplace/run_regimen.sh
+++ src_lbm/binary/validation/young_laplace/run_regimen.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 #setup from the perspective of the folder it is run in
-amrex_exec="../../../spatially_independent/main3d.gnu.MPI.ex"
+amrex_exec="../../../../../spatially_independent/main3d.gnu.MPI.ex"
 
 # Defines the radius of the droplet as a proportion of system size. 0.5 is maximum
-droplet_radii=("0.2" "0.25" "0.3" "0.35" "0.4")
-
-# Define the base folder name
-base_folder="R"
+droplet_radii=("0.30" "0.35" "0.40")
+chi_s=("2.25" "2.5" "2.75" "3" "3.25" "3.5" "3.75" "4" "4.25" "4.5" "4.75" "5")
+T="0.2"
+kappa="0.03"
 
 # Define property to be looped over and edited in input file
 search_pattern1="droplet_radius_prop = 0.3"
+search_pattern2="chi = 0.45"
+search_pattern3="kappa = 0.03"
 
+for chi in "${chi_s[@]}"; do
+
+base_folder="chi_${chi}/kappa_${kappa}"
+
+if [ ! -d "$base_folder" ]; then
+  echo "$base_folder does not exist. Creating $base_folder"
+  mkdir -p "$base_folder"
+fi
+
+cd $base_folder
+echo ${PWD}
 # Loop through each radius in R
 for droplet_radius in "${droplet_radii[@]}"; do
-  folder="${base_folder}_${droplet_radius}"
+  folder="R_${droplet_radius}"
   echo "Processing folder: $folder"
   
   # Check if the folder exists. Creates folder if it does not exist
@@ -23,23 +36,30 @@ for droplet_radius in "${droplet_radii[@]}"; do
     mkdir -p "$folder"
   fi
 
-  cp inputs* $folder
+  cp ../../inputs_young-laplace $folder
 
   # Enters folder and executes commands before returning to parent directory
-  cd "$folder"
+  cd $folder
   # Checks if run is complete and executes a run with appropriate modifications if it has not
-  if [ -e "chk_hydro_0000100000" ]; then
+  if [ -e "checkpoint_000075000" ]; then
     echo "Run complete"
+    cd ..
     continue
   else
     echo "Executing commands in $folder"
     # EDIT COMMANDS HERE TO MAKE MODIFICATIONS TO RUNS #
     replacement_text="droplet_radius_prop = $droplet_radius"
     sed -i "s/$search_pattern1/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
+    lambda=$(echo "$chi * $T" | bc -l)
+    replacement_text="chi = ${lambda}"
+    sed -i "s/$search_pattern2/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
+    replacement_text="kappa = ${kappa}"
+    sed -i "s/$search_pattern2/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
 
     mpirun -n 8 $amrex_exec inputs_young-laplace > young_laplace.txt
     # EDIT COMMANDS HERE TO MAKE MODIFICATIONS TO RUNS #
   fi
   cd ..
-
+done
+cd ../..
 done
