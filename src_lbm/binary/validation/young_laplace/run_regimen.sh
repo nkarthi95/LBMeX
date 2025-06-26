af43dff3e736955767208a106837f32c2bd45b30
Nikhil Karthikeyan <nkarthi@uschill-lab1.cis.udel.edu>
2025-06-26 12:53:02 -0400

    modified number of cores used for surface tension validation

--- src_lbm/binary/validation/young_laplace/run_regimen.sh
+++ src_lbm/binary/validation/young_laplace/run_regimen.sh
@@ -7,13 +7,14 @@ amrex_exec="../../../../../../spatially_independent/main3d.gnu.MPI.ex"
 # Defines the radius of the droplet as a proportion of system size. 0.5 is maximum
 droplet_radii=("0.30" "0.35" "0.40")
 chi_s=("2.05" "2.1" "2.15" "2.2" "2.25" "2.3" "2.35" "2.45" "2.5") # "3.5" "3.75" "4" "4.25" "4.5" "4.75" "5")
-T="0.2"
+T="0.3"
 kappa="0.01"
 
 # Define property to be looped over and edited in input file
 search_pattern1="droplet_radius_prop = 0.3"
 search_pattern2="chi = 0.45"
 search_pattern3="kappa = 0.01"
+search_pattern4="T = 0.2"
 
 for chi in "${chi_s[@]}"; do
 
@@ -42,7 +43,7 @@ for droplet_radius in "${droplet_radii[@]}"; do
   # Enters folder and executes commands before returning to parent directory
   cd $folder
   # Checks if run is complete and executes a run with appropriate modifications if it has not
-  if [ -e "checkpoint_000100000" ]; then
+  if [ -e "checkpoint_000200000" ]; then
     echo "Run complete"
     cd ..
     continue
@@ -55,9 +56,11 @@ for droplet_radius in "${droplet_radii[@]}"; do
     replacement_text="chi = ${lambda}"
     sed -i "s/$search_pattern2/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
     replacement_text="kappa = ${kappa}"
-    sed -i "s/$search_pattern2/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
+    sed -i "s/$search_pattern3/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
+    replacement_text="T = ${T}"
+    sed -i "s/$search_pattern4/$replacement_text/g" inputs_young-laplace # edits input file with appropriate setting
 
-    mpirun -n 8 $amrex_exec inputs_young-laplace > young_laplace.txt
+    mpirun -n 2 $amrex_exec inputs_young-laplace > young_laplace.txt
     # EDIT COMMANDS HERE TO MAKE MODIFICATIONS TO RUNS #
   fi
   cd ..
