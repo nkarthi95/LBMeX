ae79f4e6c108217998f584754bdcd7c564f8ee71
Nikhil Karthikeyan <nkarthi@uschill-lab1.cis.udel.edu>
2025-09-22 23:24:02 -0400

    added way to change density in addition to order parameter

--- src_lbm/binary/main_driver.cpp
+++ src_lbm/binary/main_driver.cpp
@@ -59,8 +59,8 @@ inline void ReadInput() {
   pp.query("droplet_radius_prop", droplet_radius_prop);
   pp.query("phi_in", phi_in);
   pp.query("phi_out", phi_out);
-  // pp.query("rho_in", rho_in);
-  // pp.query("rho_out", rho_out);
+  pp.query("rho_in", rho_in);
+  pp.query("rho_out", rho_out);
 
   /* time stepping */
   pp.query("nsteps", nsteps);
